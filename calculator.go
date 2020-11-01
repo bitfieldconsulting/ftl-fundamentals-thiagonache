7f2368a002e59cfda6df092ead7dd19dec31e485
Thiago Carvalho <thiagonbcarvalho@gmail.com>
2020-11-01 11:49:28 -0300

    Uses my own squareroot function instead of math.Sqrt. Also changed logic to return happy path on CalculateString

--- calculator.go
+++ calculator.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"go/token"
 	"go/types"
-	"math"
 	"regexp"
 	"strconv"
 	"strings"
@@ -42,18 +41,30 @@ func Multiply(a, b float64, extra ...float64) float64 {
 
 // Divide takes two or more numbers and returns the result of dividing the first by the second and subsequent numbers, or an error if division by zero occurs.
 func Divide(a, b float64, extra []float64) (float64, error) {
-	var divideByZeroError = "Cannot divide by zero"
+	ErrDividebyzero := errors.New("Cannot divide by zero")
 	if b == 0 {
-		return 0, errors.New(divideByZeroError)
+		return 0, ErrDividebyzero
 	}
 	result := a / b
 	for _, n := range extra {
 		if n == 0 {
-			return 0, errors.New(divideByZeroError)
+			return 0, ErrDividebyzero
 		}
 		result /= n
 	}
 	return result, nil
+}
+
+func sqrt(x float64) float64 {
+	z := 1.0
+	// First guess
+	z -= (z*z - x) / (2 * z)
+	// Iterate until change is very small
+	for zNew, delta := z, z; delta > 0.00000001; z = zNew {
+		zNew -= (zNew*zNew - x) / (2 * zNew)
+		delta = z - zNew
+	}
+	return z
 }
 
 // Sqrt takes a number and returns its square root, or an error if the number is negative.
@@ -62,31 +73,29 @@ func Sqrt(a float64) (float64, error) {
 	if a < 0 {
 		return 0, fmt.Errorf("bad input %f:  square root of a negative number is not defined", a)
 	}
-	return math.Sqrt(a), nil
+	return sqrt(a), nil
 }
 
-const regularExpression = `^(\d+)(\.\d+)?(\*|\/|\+|\-)(\d+)(\.\d+)?$`
-
-var regex = regexp.MustCompile(regularExpression)
+var paramsControl = regexp.MustCompile(`^(\d+)(\.\d+)?(\*|\/|\+|\-)(\d+)(\.\d+)?$`)
 
 // CalculateString takes math formula as string and returns the result in float64 format
 func CalculateString(input string) (float64, error) {
 
-	noSpace := strings.Replace(input, " ", "", -1)
-	match, _ := regexp.Match(regularExpression, []byte(noSpace))
-	if match {
-		parsed := regex.Find([]byte(noSpace))
-		fs := token.NewFileSet()
-		tr, err := types.Eval(fs, nil, token.NoPos, string(parsed))
-		if err != nil {
-			fmt.Printf("Cannot evaluate expression %s: %e", string(parsed), err)
-		}
-		evaluated, err := strconv.ParseFloat(tr.Value.String(), 64)
-		if err != nil {
-			fmt.Printf("Cannot convert %q from string to float64: %e", tr.Value.String(), err)
-		}
-		return evaluated, nil
+	noSpace := strings.ReplaceAll(input, " ", "")
+	match := paramsControl.Match([]byte(noSpace))
+	if !match {
+		return 0, errors.New("Invalid expression")
 	}
-	return -1, errors.New("Invalid expression")
+	parsed := paramsControl.Find([]byte(noSpace))
+	fs := token.NewFileSet()
+	tr, err := types.Eval(fs, nil, token.NoPos, string(parsed))
+	if err != nil {
+		fmt.Printf("Cannot evaluate expression %s: %e", string(parsed), err)
+	}
+	evaluated, err := strconv.ParseFloat(tr.Value.String(), 64)
+	if err != nil {
+		fmt.Printf("Cannot convert %q from string to float64: %e", tr.Value.String(), err)
+	}
 
+	return evaluated, nil
 }
